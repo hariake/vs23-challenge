0a53f9759ee59911e42ed71159b142d1c33406b0
hariake <tosarv@gmail.com>
2024-04-05 10:20:33 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-const App = () => {
- return (
-    <>
-     <h1>Food Order App</h1>
-    </>
+import React from 'react';
+import Header from './components/Header'; // Path adjusted to components folder
+import Meals from './components/Meals'; // Path adjusted to components folder
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <h1>Welcome to the React Food Order App</h1>
+        <Meals />
+        { /* Other components or content */ }
+      </main>
+    </div>
   );
 }
 
